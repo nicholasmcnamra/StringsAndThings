977cdce200c6dea8046d189396b20c3f7427a04e
nicholasmcnamra <nikomcnamara@gmail.com>
2024-03-06 16:38:40 -0500

    finished

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -88,6 +88,13 @@ public class StringsAndThings {
      *            countTriple("a") // Should return 0
      */
     public Integer countTriple(String input){
-        return null;
+        int count = 0;
+
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == input.charAt(i - 1) && input.charAt(i) == input.charAt(i + 1)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
